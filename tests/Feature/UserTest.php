95a4b3b1c7dcce3460ce5808a847143035a84efa
Nicolas Bisson <niclas035@hotmail.com>
2017-02-16 15:24:10 -0500

    Début des tests (pas nécessairement fonctionnel)

---
+++ tests/Feature/UserTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\User;
+use Illuminate\Support\Facades\Schema;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class UserTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    /**
+     * Check if the table exist.
+     *
+     * @test
+     */
+    public function check_table_users()
+    {
+        $this->assertTrue(Schema::hasTable('users'), "'users' table didn't exist.");
+    }
+
+    /**
+     * Check if it's possible to save a user model in the database.
+     *
+     * @test
+     */
+    public function save_user_model()
+    {
+        $user = new User;
+        $user->first_name = "Alexandre";
+        $user->last_name = "Caron";
+        $user->email = "alex.caron@gmail.com";
+        $user->password = bcrypt("LaMachineCaron");
+        $this->assertTrue($user->save());
+        $this->seeInDatabase('users', [
+            'id' => $user->id,
+            'first_name' => $user->first_name,
+            'last_name' => $user->last_name,
+            'email' => $user->email,
+            'password' => $user->password
+            ]);
+    }
+}
