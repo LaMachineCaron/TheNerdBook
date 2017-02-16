ad8ee61c76aa30f305e01ee94483ad046b2e0bcb
Nicolas Bisson <niclas035@hotmail.com>
2017-02-16 14:15:55 -0500

    Correction factorie user

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -21,7 +21,7 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
 
     return [
         'first_name' => $faker->firstName,
-        'last_name' => $faker->lastName,
+        'name' => $faker->lastName,
         'email' => $faker->unique()->safeEmail,
         'password' => $password ?: $password = bcrypt('secret'),
         'remember_token' => str_random(10),
