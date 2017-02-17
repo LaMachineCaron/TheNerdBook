29a7b63f2990fde55b9f95b88afc135628b956f1
XDki113r <guillaumejean46@gmail.com>
2017-02-17 15:42:19 -0500

    Now stores the refresh token into the database. Cleaned up code.

--- app/Http/Controllers/YoutubeController.php
+++ app/Http/Controllers/YoutubeController.php
@@ -2,11 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use Auth;
 use Google_Client;
 use Illuminate\Http\Request;
 use GuzzleHttp\Client;
 
+/**
+ * Class YoutubeController
+ *
+ * Controller for the Youtube API Responses
+ *
+ * @package App\Http\Controllers
+ * @author ZeLarpMaster
+ */
 class YoutubeController extends Controller {
+
+	/**
+	 * Callback for the Youtube API
+	 * Authenticates the user and saves his refresh token
+	 *
+	 * @param Request  $request  Response from the Youtube API
+	 * @return \Illuminate\Http\RedirectResponse  Back to where the user was
+	 */
 	public function callback(Request $request) {
 		$client = new Google_Client();
 		$client->setHttpClient(new Client(array(
@@ -16,6 +33,8 @@ class YoutubeController extends Controller {
 		$client->setRedirectUri(route('youtubeCallback'));
 		$client->setAccessType("offline");        // offline access
 		$client->authenticate($request->input('code'));
+		Auth::user()->token_youtube = $client->getRefreshToken();
+		Auth::user()->save();
 		return redirect()->back();
 	}
 }
--- app/Http/Traits/YoutubeTrait.php
+++ app/Http/Traits/YoutubeTrait.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Traits;
 
+use Auth;
 use Google_Client;
 use Google_Service_YouTube;
 
@@ -11,8 +12,16 @@ use Google_Service_YouTube;
  * Class containing useful methods for using the Youtube API
  *
  * @package App\Http\Traits
+ * @author ZeLarpMaster
  */
 trait YoutubeTrait {
+	/**
+	 * Generate and return an url to log into Youtube
+	 * The url automatically sets the refresh token and
+	 * 		goes back to where the user was
+	 *
+	 * @return string  The URL for the Youtube connection
+	 */
 	public function generateUrl() {
 		$client = new Google_Client();
 		$client->setAuthConfig(public_path('client_secrets.json'));
@@ -21,5 +30,14 @@ trait YoutubeTrait {
 		$client->addScope(Google_Service_YouTube::YOUTUBE_READONLY);
 		$client->setRedirectUri(route('youtubeCallback'));
 		return $client->createAuthUrl();
+	}
+
+	/**
+	 * Check whether or not the currently logged in user has a connection token
+	 *
+	 * @return bool  True if the user has a refresh token
+	 */
+	public function isLoggedInYoutube() {
+		return Auth::user()->token_youtube !== null;
 	}
 }
