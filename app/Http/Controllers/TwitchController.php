7a07a4887aec530543fb7d0912e69e686d15822b
XDki113r <guillaumejean46@gmail.com>
2017-02-21 15:30:38 -0500

    Renamed Twitch callback route and added method to get the Twitch client

--- app/Http/Controllers/TwitchController.php
+++ app/Http/Controllers/TwitchController.php
@@ -2,13 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Traits\TwitchTrait;
 use Auth;
-use GuzzleHttp\Client;
 use Illuminate\Http\Request;
-use Zarlach\TwitchApi\API\Authentication;
 
 class TwitchController extends Controller
 {
+	use TwitchTrait;
+
 	/**
 	 * Callback for the Twitch authentication
 	 *
@@ -16,7 +17,7 @@ class TwitchController extends Controller
 	 * @return \Illuminate\Http\RedirectResponse  Goes back
 	 */
 	public function loginTwitch(Request $request){
-		$token = $this->auth($request['code']);
+		$token = $this->authTwitch($request['code']);
 
 		$user = Auth::user();
 
@@ -25,48 +26,5 @@ class TwitchController extends Controller
 		$user->save();
 
 		return redirect()->back();
-	}
-
-	/**
-	 * Authenticates the user on the Twitch API using a code
-	 *
-	 * @param string  $code  The Twitch API code for authentication
-	 * @return array  Array containing the refresh token and access token
-	 */
-	private function auth($code){
-		$auth = new Authentication();
-
-		// GuzzleHttp Client with default parameters.
-		$client = new Client([
-			'base_uri' => 'https://api.twitch.tv/kraken/',
-			"verify" => false
-		]);
-
-		$options = [
-			'client_secret' => config('twitch-api.client_secret'),
-			'grant_type' => 'authorization_code',
-			'code' => $code,
-			'state' => null,
-			'redirect_uri' => config('twitch-api.redirect_url'),
-		];
-
-		$path = $auth->generateUrl('oauth2/token', false, $options, []);
-
-		// Headers
-		$data = [
-			'headers' => [
-				'Client-ID' => $auth->getClientId(),
-				'Accept' => 'application/vnd.twitchtv.v3+json',
-			],
-		];
-
-		// Request object
-		$request = new \GuzzleHttp\Psr7\Request("POST", $path, $data);
-
-		// Send request
-		$response = $client->send($request);
-
-		// Return body in JSON data
-		return json_decode($response->getBody(), true);
 	}
 }
