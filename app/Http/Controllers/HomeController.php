7f93fa15700d8ef2170517d38200a98125454084
Simon Gagné <simgagne12@gmail.com>
2017-02-23 14:52:46 -0500

    Début de la création des posts

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -61,6 +61,36 @@ use TwitchTrait;
         return view('home', compact('data'));
     }
 
+    public function create_post($request, $stream, $video)
+    {
+        $data = $request->all();
+        if (isset($stream)) {
+            $post = new Post();
+            $post->user_id = Auth::user()->id;
+            $post->type = 1;
+            $post->caption = $data['caption'];
+            $post->title = $stream['channel']['status'];
+            $post->channel_name = $stream['channel']['name']; //Not using display_name
+            $post->game_title = $stream['game'];
+        }
+        if (isset($video)) {
+            $post = new Post();
+            $post->user_id = Auth::user()->id;
+            $post->type = 2;
+            $post->caption = $data['caption'];
+            $post->title = $video['title'];
+            $post->channel_name = $video['name']; //Not using display_name
+            $post->url = $video['url'];
+        }
+        if (isset($post)) {
+            if ($post->save()) {
+                return redirect()->back()->with('status', 'Le post a été créé.');
+            } else {
+                return redirect()->back()->withErrors('Erreur de sauvegarde du post.');
+            }
+        }
+    }
+
     public function test()
     {
         $input = Request::input('search');
