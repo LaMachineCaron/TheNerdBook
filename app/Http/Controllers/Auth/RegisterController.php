0c4672ffca32bc91b817c6c5f4d25af7008a6feb
Marc <marc-plante1@hotmail.com>
2017-02-23 15:35:58 -0500

    register / forgotpassword

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -27,7 +27,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/home/page/1';
 
     /**
      * Create a new controller instance.
